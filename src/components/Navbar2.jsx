c511711bdfc36f4a45243c16a079363423995e1c
love.to.code108 <love.to.code108@gmail.com>
2024-04-09 02:23:26 +0530

    go back button

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 
 
 
@@ -15,12 +15,21 @@ export const Navbar2 = () => {
     }
 
 
+    const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  }
+
+
 
     return (
         <h1 className=" text-2xl ml-4 mt-2">
             {/* so to link anything we dont have to give the element in attributes but only just the path using the to attribute */}
             <NavLink style={navLinkStyles} className=" mr-4 p-2" to="/"> Home </NavLink>
             <NavLink style={navLinkStyles} className=" p-2" to="/about"> About </NavLink>
+
+            <button onClick={goBack}> Go Back </button>
         </h1>
     )
 }
--- src/components/OrdersSummary.jsx
+++ src/components/OrdersSummary.jsx
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom"
 
 
 export const OrdersSummary = () => {
+
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  }
+
+
+
+
   return (
     <div>
         <h1 className=' text-3xl'>Order Confirmed</h1>
+        <button onClick={goBack}> Go Back </button>
     </div>
   )
 }
