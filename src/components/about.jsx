892b2db8756f5f3a8af95a14a0fb6d21723d1e16
love.to.code108 <love.to.code108@gmail.com>
2024-04-09 01:53:47 +0530

    browser router routes route

---
+++ src/components/about.jsx
@@ -0,0 +1,6 @@
+
+export const About = () => {
+  return (
+    <div><h1>about</h1></div>
+  )
+}
---
+++ src/components/error.jsx
@@ -0,0 +1,6 @@
+
+export const FourOFour = () => {
+    return (
+        <div><h1>404</h1></div>
+    )
+}
